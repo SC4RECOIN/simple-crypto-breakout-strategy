d0cc3c5255d7857d5bd7e6d7ad1aa6ef05783d3e
Kurtis Streutker <kurtisstreutker@gmail.com>
2021-04-23 17:21:47 -0400

    remove ts project

--- src/exchange/abstract.ts
+++
@@ -1,9 +0,0 @@
-import {Candle} from '../entity/candle';
-import moment from 'moment';
-
-abstract class Exchange {
-  abstract candles(symbol: string, start: moment.Moment): Promise<Candle[]>;
-  abstract perpetuals(minVolume?: number): Promise<string[]>;
-}
-
-export default Exchange;
