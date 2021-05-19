fed0242267c37c4eead532227a487d752aab5130
Kurtis Streutker <kurtisstreutker@gmail.com>
2021-05-18 22:20:41 -0400

    pwa start

--- dashboard/src/index.tsx
+++ dashboard/src/index.tsx
@@ -8,6 +8,7 @@ import Positions from "./components/Positions";
 import { isMobile } from "react-device-detect";
 import Stats from "./components/Stats";
 import SafetySwitches from "./components/Safety";
+import * as serviceWorkerRegistration from "./serviceWorker/registration";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -34,3 +35,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+serviceWorkerRegistration.register();
