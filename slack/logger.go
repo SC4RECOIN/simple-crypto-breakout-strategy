8a6cc7c8774573cd3f49befa172db213d6df26c7
Kurtis Streutker <kurtisstreutker@gmail.com>
2021-06-30 20:52:51 -0400

    reduce console logs

--- slack/logger.go
+++ slack/logger.go
@@ -7,12 +7,10 @@ import (
 )
 
 func LogInfo(message string) {
-	fmt.Println("log:", message)
 	DefaultClient().PostMessage(models.LogChannel, message)
 }
 
 func LogError(err error) {
 	message := fmt.Sprintf("`An error occured: %v`\n", err.Error())
-	fmt.Println("error:", message)
 	DefaultClient().PostMessage(models.LogChannel, message)
 }
